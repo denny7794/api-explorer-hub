16eeefba439a98259047f4697181b9df7ccafa6b
Denis Pi. <zdeniz@mail.ru>
2024-10-14 13:17:21 +0300

    2 controllers: prepod solution

--- Controller/HelloController.cs
+++ Controller/HelloController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 // http://localhost:5066/api/Name
 [ApiController]
 [Route("api/[controller]")]
-public class NameController : ControllerBase
+public class HelloController : ControllerBase
 {
     // app.MapGet("/hello/{name}", (string name) => $"Привет, {name}!");
     // http://localhost:5066/api/Name/{name}

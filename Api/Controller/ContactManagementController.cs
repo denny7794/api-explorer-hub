9f9e1cd26b28a2a8532e3e063a543820ec0733ea
Denis Pi. <zdeniz@mail.ru>
2024-10-25 13:03:14 +0300

    метод получения контактов

--- Api/Controller/ContactManagementController.cs
+++ Api/Controller/ContactManagementController.cs
@@ -14,4 +14,10 @@ public class ContactManagementController : BaseController
     {
         storage.Contacts.Add(contact);
     }
+
+    [HttpGet("contacts")]
+    public List<Contact> GetContacts()
+    {
+        return storage.Contacts;
+    }
 }

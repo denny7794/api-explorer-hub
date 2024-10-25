177ef52e0e130b97d7ee3d4a44dd0854f37f115b
Denis Pi. <zdeniz@mail.ru>
2024-10-25 13:34:10 +0300

    генерация тестовых данных

--- Api/Storage/Contact.Storage.cs
+++ Api/Storage/Contact.Storage.cs
@@ -3,7 +3,18 @@ public class ContactStorage
     public ContactStorage()
     {
         this.Contacts = new List<Contact>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            this.Contacts.Add(new Contact()
+            {
+                Id = i,
+                Name = $"Полное имя {i}",
+                Email = $"{Guid.NewGuid().ToString().Substring(0, 5)}_{i}@pochtovik.ru",
+            });
+        }
     }
+
 
     public List<Contact> Contacts { get; set; }
 }

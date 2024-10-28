37b51c87c3fee198099695fdcba2138b6bf5acbc
Denis Pi. <zdeniz@mail.ru>
2024-10-28 09:52:55 +0300

    удаление лишнего для новой главы

--- Api/Storage/DataContext.cs
+++
@@ -1,9 +0,0 @@
-//это правильный способ для хранения данных
-public class DataContext
-{
-    public DataContext()
-    {
-        this.Str = String.Empty;
-    }
-    public string Str { get; set; }
-}

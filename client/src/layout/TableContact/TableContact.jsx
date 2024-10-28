6937eeaa0bcfaf72a62e4cbddf3c0148cadf1020
Denis Pi. <zdeniz@mail.ru>
2024-10-28 14:19:46 +0300

    front: передача данных чз св-ва 3

--- client/src/layout/TableContact/TableContact.jsx
+++ client/src/layout/TableContact/TableContact.jsx
@@ -4,7 +4,6 @@ import RowTableContact from './components/RowTableContact';
 
 
 const TableContact = (props) => {
-    const contacts = props.contacts;
     return (
         <div>
             <table className="table table-hover">
@@ -16,16 +15,16 @@ const TableContact = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    <RowTableContact
-                        id={contacts[0].id}
-                        name={contacts[0].name}
-                        email={contacts[0].email}
-                    />
-                    <RowTableContact
-                        id={contacts[1].id}
-                        name={contacts[1].name}
-                        email={contacts[1].email}
-                    />
+                    {
+                        props.contacts.map(
+                            contact =>
+                            (<RowTableContact
+                                id={contact.id}
+                                name={contact.name}
+                                email={contact.email}
+                            />)
+                        )
+                    }
                 </tbody>
             </table>
         </div>

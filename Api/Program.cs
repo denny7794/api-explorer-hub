898ff26081b024fd5e37d3a6c31a003a6c165606
Denis Pi. <zdeniz@mail.ru>
2024-10-29 13:15:11 +0300

    front+back: добавление политик CORS

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,11 +5,21 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ContactStorage>();
 
+// добавляем политику CORS для доступа к API
+builder.Services.AddCors(opt =>
+opt.AddPolicy("CorsPolicy", policy =>
+{
+    policy.AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithOrigins("http://localhost:3000"); // с какого адреса разрешен доступ
+}));
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+app.UseCors("CorsPolicy"); // применяем политику CORS
 app.Run();
 
 

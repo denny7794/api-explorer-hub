bc0e53b0855e9e1b09923b3bfc07823ee9771eec
Denis Pi. <zdeniz@mail.ru>
2024-10-29 09:29:24 +0300

    front: добавление react-хука

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
+import React, { useState } from "react";
 import TableContact from "./layout/TableContact/TableContact";
 
-const contacts = [
-  { id: 1, name: 'Иван 1', email: 'ivan@mail1.ru' },
-  { id: 2, name: 'Иван 2', email: 'ivan@mail2.ru' },
-  { id: 3, name: 'Иван 3', email: 'ivan@mail3.ru' },
-];
-
-const addContact = () => {
-  const item = {
-    id: Math.floor(Math.random() * 100),
-    name: 'Иван 3',
-    email: 'ivan@mail3.ru'
-  }
-  contacts.push(item);
-  console.log(contacts);
-}
 
 const App = () => {
+  const [contacts, setContacts] = useState(
+    [
+      { id: 1, name: 'Иван 1', email: 'ivan@mail1.ru' },
+      { id: 2, name: 'Иван 2', email: 'ivan@mail2.ru' },
+      { id: 3, name: 'Иван 3', email: 'ivan@mail3.ru' },
+    ]
+  );
+
+  const addContact = () => {
+    const item = {
+      id: Math.floor(Math.random() * 100),
+      name: 'Иван 3',
+      email: 'ivan@mail3.ru'
+    }
+    setContacts([...contacts, item]);
+    console.log(contacts);
+  }
+
   return (
     <div className="container mt-5">
       <div className="card">

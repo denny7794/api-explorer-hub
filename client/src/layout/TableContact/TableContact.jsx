987c192eade6b5a986e4b7d0c6abe746f44bb26c
Denis Pi. <zdeniz@mail.ru>
2024-10-29 12:14:26 +0300

    front: добавление уникального свойства (key)

--- client/src/layout/TableContact/TableContact.jsx
+++ client/src/layout/TableContact/TableContact.jsx
@@ -19,6 +19,7 @@ const TableContact = (props) => {
                         props.contacts.map(
                             contact =>
                             (<RowTableContact
+                                key={contact.id}
                                 id={contact.id}
                                 name={contact.name}
                                 email={contact.email}
